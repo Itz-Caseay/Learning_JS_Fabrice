a902e30a2c4cde0ca6ec746fe9c7ad96348666de
Itz-Caseay <gred01485@gmail.com>
2025-07-01 17:22:49 +0100

    Merge Day2 contents into main

---
+++ Day2_Js/index.js
@@ -0,0 +1,16 @@
+// Performing a task
+
+function greet (name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+// Calculate a value
+
+function square(number) {
+    return number * number;
+}
+
+
+greet('John', 'Smith');
+
+console.log(square(2000));
