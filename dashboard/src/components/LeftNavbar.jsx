f3e19d80afabc02887b5a97c6292463413b59a84
Itz-Caseay <gred01485@gmail.com>
2025-08-26 14:49:28 +0100

    new update

---
+++ dashboard/src/components/LeftNavbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles/LeftNavbar.css';
+
+
+const LeftNavbar = () => {
+  return (
+    <div>
+        <div className="navbar">
+                <div className="logo">Let's Bleed</div>
+        </div>
+    </div>
+  )
+}
+
+export default LeftNavbar
