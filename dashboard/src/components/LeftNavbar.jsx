a8c738d137be7c310a1fca1e8f9c3c8d939ebe6d
Itz-Caseay <gred01485@gmail.com>
2025-08-26 15:27:56 +0100

    new update8

--- dashboard/src/components/LeftNavbar.jsx
+++ dashboard/src/components/LeftNavbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './styles/LeftNavbar.css';
 import { Hexagon } from 'lucide-react';
+import {Dashboard} from 'lucide-react';
 
 
 const LeftNavbar = () => {
   return (
     <div>
         <div className="navbar">
-                <div className="logo"> <Hexagon size={50} /> <span>CORE <span>UI</span> <h4>REACT.JS</h4></span></div>
+                <div className="logo"> <Hexagon size={45} /> <span>CORE <span>UI</span> <h4>REACT.JS</h4></span></div>
+                <div className='dashbord-btn'><Dashboard /> Dashboard</div>
         </div>
     </div>
   )
